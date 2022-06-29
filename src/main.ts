db8edb4c8d923a95c8b3b16148d647fefaea694e
Lukasz Pietrowski <bazul603@gmail.com>
2022-06-29 19:22:58 +0200

    adding swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import helmet from 'helmet';
 
 import { AppModule } from './app.module';
@@ -12,6 +13,19 @@ async function bootstrap() {
   app.use(helmet());
 
   const configService = app.get(ConfigService);
+
+  const ev = configService.get('NODE_ENV');
+
+  if (ev === 'development') {
+    const config = new DocumentBuilder()
+      .setTitle('App Endpoints')
+      .setDescription('posible endpoints app')
+      .setVersion('1.0')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('swagger', app, document);
+  }
 
   const PORT = configService.get('PORT');
   await app.listen(PORT);
